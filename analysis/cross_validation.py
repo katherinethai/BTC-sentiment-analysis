82ff28f1294533f001b2b6d9b253ee69f12d495a
katherinethai <kthai1618@gmail.com>
2019-01-11 01:47:14 -0500

    added different models + neg feature

--- analysis/cross_validation.py
+++ analysis/cross_validation.py
@@ -3,7 +3,8 @@ import re
 import pandas
 import nltk
 import json
-from preprocessing import get_features1
+from preprocessing import get_features4
+from preprocessing import get_features4
 from nltk.corpus import wordnet 
 import sys
 reload(sys)
@@ -28,14 +29,14 @@ with open('../data/json/test3.json') as f:
   test_headlines3 = json.load(f)
 
 
-train_set1 = [(get_features1(h), kind) for (h, kind) in train_headlines1]
-test_set1 =  [(get_features1(h), kind) for (h, kind) in test_headlines1]
+train_set1 = [(get_features4(h), kind) for (h, kind) in train_headlines1]
+test_set1 =  [(get_features4(h), kind) for (h, kind) in test_headlines1]
 
-train_set2 = [(get_features1(h), kind) for (h, kind) in train_headlines2]
-test_set2 =  [(get_features1(h), kind) for (h, kind) in test_headlines2]
+train_set2 = [(get_features4(h), kind) for (h, kind) in train_headlines2]
+test_set2 =  [(get_features4(h), kind) for (h, kind) in test_headlines2]
 
-train_set3 = [(get_features1(h), kind) for (h, kind) in train_headlines3]
-test_set3 =  [(get_features1(h), kind) for (h, kind) in test_headlines3]
+train_set3 = [(get_features4(h), kind) for (h, kind) in train_headlines3]
+test_set3 =  [(get_features4(h), kind) for (h, kind) in test_headlines3]
 
 classifier1 = nltk.NaiveBayesClassifier.train(train_set1)
 acc1 = (nltk.classify.accuracy(classifier1, test_set1))*100
@@ -52,42 +53,53 @@ print("Classifier accuracy percent:" + str(acc3))
 print ((acc1 + acc2 + acc3)/3)
 
 # # Error Analysis
-errors = []
-for (headline,tag) in test_headlines1:
-  guess = classifier1.classify(get_features1(headline))
-  if guess != tag:
-    errors.append( (tag, guess, headline) )
-
-for (tag, guess, headline) in sorted(errors):
-    print('correct={:<8} guess={:<8s} headline={:<30}'.format(tag, guess, headline))
-
-# print ("--------")
-
-# for (headline,tag) in test_headlines2:
-#   guess = classifier2.classify(get_features1(headline))
-#   if guess != tag:
-#     errors.append( (tag, guess, headline) )
-
-# for (tag, guess, headline) in sorted(errors):
-#     print('correct={:<8} guess={:<8s} headline={:<30}'.format(tag, guess, headline))
-
-# print ("--------")
-
-# for (headline,tag) in test_headlines3:
-#   guess = classifier3.classify(get_features1(headline))
-#   if guess != tag:
-#     errors.append( (tag, guess, headline) )
-
-# for (tag, guess, headline) in sorted(errors):
-#     print('correct={:<8} guess={:<8s} headline={:<30}'.format(tag, guess, headline))
-
 def tag_list(dataset):
   return [tag for (headline, tag) in dataset]
 def apply_tagger(dataset, classifier):
-  return [classifier.classify(get_features1(headline)) for (headline,tag) in dataset]
+  return [classifier.classify(get_features4(headline)) for (headline,tag) in dataset]
 
+# errors1 = []
+# for (headline,tag) in test_headlines1:
+#   guess = classifier1.classify(get_features4(headline))
+#   if guess != tag:
+#     errors1.append( (tag, guess, headline) )
 
-gold = tag_list(test_headlines1)
-test = apply_tagger(test_headlines1,classifier1)
-cm = nltk.ConfusionMatrix(gold, test)
-print(cm.pretty_format(sort_by_count=True, show_percents=True, truncate=9))
+# for (tag, guess, headline) in sorted(errors1):
+#     print('correct={:<8} guess={:<8s} headline={:<30}'.format(tag, guess, headline))
+
+# print ("--------")
+
+# errors2 = []
+# for (headline,tag) in test_headlines2:
+#   guess = classifier2.classify(get_features4(headline))
+#   if guess != tag:
+#     errors2.append( (tag, guess, headline) )
+
+# for (tag, guess, headline) in sorted(errors2):
+#     print('correct={:<8} guess={:<8s} headline={:<30}'.format(tag, guess, headline))
+
+# print ("--------")
+
+# errors3 = []
+# for (headline,tag) in test_headlines3:
+#   guess = classifier3.classify(get_features4(headline))
+#   if guess != tag:
+#     errors3.append( (tag, guess, headline) )
+
+# for (tag, guess, headline) in sorted(errors3):
+#     print('correct={:<8} guess={:<8s} headline={:<30}'.format(tag, guess, headline))
+
+gold1 = tag_list(test_headlines1)
+test1 = apply_tagger(test_headlines1,classifier1)
+cm1 = nltk.ConfusionMatrix(gold1, test1)
+print(cm1.pretty_format(sort_by_count=True, show_percents=True, truncate=9))
+
+gold2 = tag_list(test_headlines2)
+test2 = apply_tagger(test_headlines2,classifier2)
+cm2 = nltk.ConfusionMatrix(gold2, test2)
+print(cm2.pretty_format(sort_by_count=True, show_percents=True, truncate=9))
+
+gold3 = tag_list(test_headlines3)
+test3 = apply_tagger(test_headlines3,classifier3)
+cm3 = nltk.ConfusionMatrix(gold3, test3)
+print(cm3.pretty_format(sort_by_count=True, show_percents=True, truncate=9))
--- analysis/preprocessing.py
+++ analysis/preprocessing.py
@@ -5,24 +5,23 @@ import nltk
 from nltk.tokenize import word_tokenize, RegexpTokenizer
 from nltk.corpus import stopwords
 from nltk.corpus import wordnet 
+from nltk.stem.lancaster import LancasterStemmer
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 stop_words = stopwords.words('english')
-tokenizer = RegexpTokenizer(r'\w+')
+stop_words.extend([''])
+exceptions = set(('no', 'nor', 'not','below','above','against','after','up','down','over','under','again','more','few'))
+stop = set(stop_words) - exceptions
 
-synonyms = []
-antonyms = []
+tokenizer = RegexpTokenizer(r"\w+(?=n\'t)|n\'t|\w+(?=')|\'\w+|\w+")
 
-for syn in wordnet.synsets("lose"): 
-    for l in syn.lemmas(): 
-        synonyms.append(l.name()) 
-        if l.antonyms(): 
-            antonyms.append(l.antonyms()[0].name()) 
-  
-print(set(synonyms)) 
-print(set(antonyms)) 
+loss_words = ['hack','regulate','ban']
+gain_words = ['launch','rise','support']
+neg_words = ['no','refuse','reject','not','nor','n\'t']
+
+lancaster_stemmer = LancasterStemmer()
 
 def get_features1(headline):
   #tokenize into words
@@ -40,5 +39,48 @@ def get_features2(headline):
   toks = [t.lower() for t in toks if t.lower() not in stop_words]
   #remove numbers
   toks = [re.sub(r'\d+','',t) for t in toks]
+  #get stems
+  toks = [lancaster_stemmer.stem(t) for t in toks]
   return dict([(word, True) for word in toks])
+
+def get_features3(headline):
+  #tokenize into words
+  toks = tokenizer.tokenize(headline)
+  #lowercase and different set of stop words
+  toks = [t.lower() for t in toks if t.lower() not in stop]
+  #remove numbers
+  toks = [re.sub(r'\d+','',t) for t in toks]
+  #get stems
+  toks = [lancaster_stemmer.stem(t) for t in toks]
+  return dict([(word, True) for word in toks])
+
+def get_features4(headline):
+  features = {}
+  #tokenize into words
+  toks = tokenizer.tokenize(headline)
+  neg_count = 0
+  #lowercase and different set of stop words
+  toks = [t.lower() for t in toks if t.lower() not in stop]
+  for t in toks:
+    #remove numbers, get stems
+    t = re.sub(r'\d+','',t)
+    t = lancaster_stemmer.stem(t)
+    #mark if there are loss words
+    if t in loss_words: 
+      loss = True 
+      neg_count += 1
+    else: 
+      loss = False
+    #mark if gain words 
+    gain = True if t in gain_words else False
+    #mark if neg words
+    if t in neg_words: neg_count += 1
+    neg = True if neg_count % 2 == 1 else False
+
+    features[t] = True
+    # features['loss_words'] = loss
+    # features['gain_words'] = gain
+    features['neg'] = neg
+
+  return features
 
